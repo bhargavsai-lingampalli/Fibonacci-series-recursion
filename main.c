f3a86c5c68743ac7e5df12d355afbce6f204cb4e
Bhargav Sai <114660786+BhargavSai-Lingampalli@users.noreply.github.com>
2023-02-24 20:04:18 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void pfibo(int n);
+int main() {
+    int n;
+
+    scanf("%d",&n);
+    printf("0\t1\t");
+    pfibo(n);
+
+    return 0;
+}
+
+void pfibo(int n)
+{
+    static a=0,b=1,c;
+    if(n!=0)
+    {
+        pfibo(n-1);
+        c=a+b;
+        printf("%d\t",c);
+        a=b;
+        b=c;
+    }
+}
